74010503fc1b787fda4f97e442613a8767ab2aa0
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-11-16 16:04:13 -0500

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -2,13 +2,13 @@ import { expect } from 'chai';
 import * as sinon from 'sinon';
 
 import moduleUnderTest from '../src/moduleUnderTest';
-import * as stubableFunction from '../src/stubee';
+import * as stubee from '../src/stubee';
 
 describe('Example stub', () => {
 	let sandbox: sinon.SinonSandbox = sinon.createSandbox();
 
 	beforeEach(() => {
-		sandbox.stub(stubableFunction, 'default').resolves('this is working');
+		sandbox.stub(stubee, 'default').resolves('this is working');
 	})
 
 	afterEach(() => {
